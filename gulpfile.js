ba0010aeefc42936313def78a476404e92d2206e
lleohao <lleohao@hotmail.com>
2017-10-25 10:56:54 +0800

    fix: 修复gulpfile中 NEJ.INDEX.js 文件位置错误

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,15 @@ const rename = require('gulp-rename');
 const nejc = require('nejc');
 const path = require('path');
 const fs = require('fs');
-const dist =  process.env.PLATFORM ? 'nej-commonjs-platform': 'nej-commonjs';
+const dist = process.env.PLATFORM ? 'nej-commonjs-platform' : 'nej-commonjs';
 const config = {
     'src': 'src/**',
     'dist': dist,
     'ignoreFiles': [
         path.join(__dirname, 'src', '**', 'test', '**'),
         path.join(__dirname, 'src', 'define.js'),
-        path.join(__dirname, 'src', 'util','query','nes.js'),
-        path.join(__dirname, 'src', 'util','template','trimpath.js')
+        path.join(__dirname, 'src', 'util', 'query', 'nes.js'),
+        path.join(__dirname, 'src', 'util', 'template', 'trimpath.js')
     ],
     'ext': ['.js'],
     'alias': {
@@ -33,26 +33,26 @@ gulp.task('clean', function () {
             platformJS,
             path.join(config.src, 'define.js'),
             path.join(config.src, '**', 'test'),
-            path.join(config.src, '**', 'demo')])
+            path.join(config.src, '**', 'demo')
+        ])
         .pipe(clean());
 });
 
 gulp.task('build', ['clean'], function () {
-	return gulp
+    return gulp
         .src(config.src)
         .pipe(nejc(config))
         .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('AddNEJPATCH',
-    ['build'],
+gulp.task('AddNEJPATCH', ['build'],
     function () {
-    var tpl = fs.readFileSync(path.join('template', 'NEJ.PATCH.js')).toString('utf-8');
-    return gulp
-        .src(platformJS)
-        .pipe(insert.append(tpl))
-        .pipe(gulp.dest(path.parse(platformJS).dir))
-});
+        var tpl = fs.readFileSync(path.join('template', 'NEJ.PATCH.js')).toString('utf-8');
+        return gulp
+            .src(platformJS)
+            .pipe(insert.append(tpl))
+            .pipe(gulp.dest(path.parse(platformJS).dir))
+    });
 
 
 gulp.task('COPYINDEXOTHERS', ['AddNEJPATCH'], function () {
@@ -67,7 +67,7 @@ gulp.task('COPYINDEXOTHERS', ['AddNEJPATCH'], function () {
 gulp.task('COPYINDEX', ['COPYINDEXOTHERS'], function () {
     return gulp
         .src([
-            './NEJ.INDEX.js'
+            path.join('template', 'NEJ.INDEX.js')
         ])
         .pipe(rename('index.js'))
         .pipe(gulp.dest(dist))
